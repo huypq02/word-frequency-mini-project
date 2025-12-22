15bb1e7a79503e81b76ea65a625fe524d5dd3eb6
Huy Pham <huypham0297@gmail.com>
2025-12-22 23:54:02 +0700

    fix: syntax showing details of error using HTTPException

--- src/app/main.py
+++ src/app/main.py
@@ -9,7 +9,7 @@ async def analyze_text(request: TextStatsRequest):
     try:
         # Validate input text
         if request.text is None or request.text.strip() == "":
-            raise HTTPException(status_code=400, details="Input text must not be empty or whitespace-only")
+            raise HTTPException(status_code=400, detail="Input text must not be empty or whitespace-only")
 
         # Preprocessing data
         token = ts.preprocessing(text=request.text)
@@ -18,10 +18,10 @@ async def analyze_text(request: TextStatsRequest):
 
         # Validate Dataframe structure
         if word_stats.empty:
-            raise HTTPException(status_code=500, details="No words found after processing data")
+            raise HTTPException(status_code=500, detail="No words found after processing data")
 
         if 'words' not in word_stats.columns:
-            raise HTTPException(status_code=500, details="Invalid Dataframe structure")
+            raise HTTPException(status_code=500, detail="Invalid Dataframe structure")
 
         return TextStatsResponse(status="success",
                                 message="Text analysis completed successfully",
@@ -39,7 +39,7 @@ async def analyze_file(file: UploadFile = File(...)):
         text = data.decode("utf-8") # Convert bytes to string
         # Validate input text
         if text.strip() == "":
-            raise HTTPException(status_code=400, details="Input text must not be empty or whitespace-only")
+            raise HTTPException(status_code=400, detail="Input text must not be empty or whitespace-only")
 
         # Preprocessing data
         token = ts.preprocessing(text=text)
@@ -48,10 +48,10 @@ async def analyze_file(file: UploadFile = File(...)):
 
         # Validate Dataframe structure
         if word_stats.empty:
-            raise HTTPException(status_code=500, details="No words found after processing data")
+            raise HTTPException(status_code=500, detail="No words found after processing data")
 
         if 'words' not in word_stats.columns:
-            raise HTTPException(status_code=500, details="Invalid Dataframe structure")
+            raise HTTPException(status_code=500, detail="Invalid Dataframe structure")
 
         return TextStatsResponse(status="success",
                                  message="Text analysis completed successfully",
