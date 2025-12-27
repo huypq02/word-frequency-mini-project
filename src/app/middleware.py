ddd20072c51e33a943c1906feba67001282c1c36
Huy Pham <huypham0297@gmail.com>
2025-12-27 23:56:15 +0700

    fix: rename API name more suitable and add allowed methods, content, media_type for middleware

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -25,7 +25,7 @@ class LimitUploadSize(BaseHTTPMiddleware):
             except ValueError:
                 print("Error while converting the content-length header to integer.")
                 return Response(status_code=status.HTTP_400_BAD_REQUEST,
-                                context='"detail":"Content-Length should be a numeric value"',
+                                content='"detail":"Content-Length should be a numeric value"',
                                 media_type="application/json")
 
             if content_length > self.max_upload_size:
