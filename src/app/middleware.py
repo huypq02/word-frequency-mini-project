caf1c82291ffb67f8d126e585b11e01916af39e2
Huy Pham <huypham0297@gmail.com>
2025-12-27 23:55:29 +0700

    fix: rename API name more suitable and add allowed methods, content, media_type for middleware

--- src/app/middleware.py
+++ src/app/middleware.py
@@ -4,16 +4,32 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.types import ASGIApp
 
+
+ALLOWED_METHOD = ['POST','PUT','PATCH']
+
 class LimitUploadSize(BaseHTTPMiddleware):
     def __init__(self, app: ASGIApp, max_upload_size: int = 5_000_000) -> None:
         super().__init__(app)
         self.max_upload_size = max_upload_size
 
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        if request.method == 'POST':
+        if request.method in ALLOWED_METHOD:
             if 'content-length' not in request.headers:
-                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED)
-            content_length = int(request.headers['content-length'])
+                return Response(status_code=status.HTTP_411_LENGTH_REQUIRED,
+                                content='"detail":"Content-Length header is required for file uploads."',
+                                media_type="application/json")
+            content_length_header = request.headers['content-length']
+            
+            try:
+                content_length = int(content_length_header)
+            except ValueError:
+                print("Error while converting the content-length header to integer.")
+                return Response(status_code=status.HTTP_400_BAD_REQUEST,
+                                context='"detail":"Content-Length should be a numeric value"',
+                                media_type="application/json")
+
             if content_length > self.max_upload_size:
-                return Response(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+                return Response(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                                content='"detail":"Uploaded content exceeds the maximum allowed size."',
+                                media_type="application/json")
         return await call_next(request)
